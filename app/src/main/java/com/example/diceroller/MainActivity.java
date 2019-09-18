5ebf0de3f5ee216d1b48e53edf7a6ea76d7e6557
Kyle Lemons <klemons15@hotmail.com>
2019-09-18 08:51:10 -0500

    added multiple dice side images

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void rollDice() {
-        int randomNumber = rng.nextInt(6) + 1;
+        int randomNumber = rng.nextInt(20) + 1;
 
         switch (randomNumber) {
             case 1:
@@ -36,16 +36,58 @@ public class MainActivity extends AppCompatActivity {
                 imageViewDice.setImageResource(R.drawable.dice2);
                 break;
             case 3:
-                imageViewDice.setImageResource(R.drawable.dice3);
+                imageViewDice.setImageResource(R.drawable.d3);
                 break;
             case 4:
-                imageViewDice.setImageResource(R.drawable.dice4);
+                imageViewDice.setImageResource(R.drawable.d4);
                 break;
             case 5:
-                imageViewDice.setImageResource(R.drawable.dice5);
+                imageViewDice.setImageResource(R.drawable.d5);
                 break;
             case 6:
-                imageViewDice.setImageResource(R.drawable.dice6);
+                imageViewDice.setImageResource(R.drawable.d6);
+                break;
+            case 7:
+                imageViewDice.setImageResource(R.drawable.d7);
+                break;
+            case 8:
+                imageViewDice.setImageResource(R.drawable.d8);
+                break;
+            case 9:
+                imageViewDice.setImageResource(R.drawable.d9);
+                break;
+            case 10:
+                imageViewDice.setImageResource(R.drawable.d10);
+                break;
+            case 11:
+                imageViewDice.setImageResource(R.drawable.d11);
+                break;
+            case 12:
+                imageViewDice.setImageResource(R.drawable.d12);
+                break;
+            case 13:
+                imageViewDice.setImageResource(R.drawable.d13);
+                break;
+            case 14:
+                imageViewDice.setImageResource(R.drawable.d14);
+                break;
+            case 15:
+                imageViewDice.setImageResource(R.drawable.d15);
+                break;
+            case 16:
+                imageViewDice.setImageResource(R.drawable.d16);
+                break;
+            case 17:
+                imageViewDice.setImageResource(R.drawable.d17);
+                break;
+            case 18:
+                imageViewDice.setImageResource(R.drawable.d18);
+                break;
+            case 19:
+                imageViewDice.setImageResource(R.drawable.d19);
+                break;
+            case 20:
+                imageViewDice.setImageResource(R.drawable.d20);
                 break;
         }
     }
