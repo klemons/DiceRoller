c627fc2468efaf6184c844d374340ad8bb7f5d8c
Kyle Lemons <klemons15@hotmail.com>
2019-10-07 10:41:17 -0500

    created text views that will display times rolled in StatsActivity

--- app/src/main/java/com/example/diceroller/StatsActivity.java
+++ app/src/main/java/com/example/diceroller/StatsActivity.java
@@ -1,5 +1,11 @@
 package com.example.diceroller;
 
-public class StatsActivity extends MainActivity {
+import android.os.Bundle;
 
+public class StatsActivity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
